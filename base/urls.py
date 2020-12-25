b4ef46ac333a440790ac09a999326ad78652d364
adrian <adrian.allkoci@gmail.com>
2020-12-25 02:08:28 +0100

    Make a CRUD for my blog

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/', views.posts, name='posts'),
-    path('post/', views.post, name='post'),
+    path('post/<str:pk>/', views.post, name='post'),
     path('profile/', views.profile, name='profile'),
+
+    # CRUD Paths
+
+    path('create_post', views.createPost, name='create_post'),
+    path('update_post/<str:pk>/', views.updatePost, name='update_post'),
+    path('delete_post/<str:pk>/', views.deletePost, name='delete_post'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,16 +1,65 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+from .forms import PostForm
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/home.html')
+    posts = Post.objects.filter(active=True, featured=True)[0:3]
+    context = {'posts':posts}
+    return render(request, 'base/home.html', context)
 
 def posts(request):
-    return render(request, 'base/posts.html')
+    posts = Post.objects.filter(active=True)
+    context = {'posts':posts}
+    return render(request, 'base/posts.html', context)
 
-def post(request):
-    return render(request, 'base/post.html')
+def post(request, pk):
+    # post = get_object_or_404(Post, id=id)
+    post = Post.objects.get(id=pk)
+    context = {'post':post}
+    return render(request, 'base/post.html', context)
 
 def profile(request):
     return render(request, 'base/profile.html')
+
+
+# CRUD Views
+
+@login_required(login_url="home")
+def createPost(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form':form}
+    return render(request, 'base/post_form.html', context)
+
+@login_required(login_url="home")
+def updatePost(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form':form}
+    return render(request, 'base/post_form.html', context)
+
+@login_required(login_url="home")
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts')
+    context = {'item':post}
+    return render(request, 'base/delete.html', context)
